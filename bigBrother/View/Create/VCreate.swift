90a1699832677c060aeba61c42df36dcbe79fbd8
vauxhall <zero.griffin@gmail.com>
2017-06-07 09:49:05 -0500

    view create bar

--- bigBrother/View/Create/VCreate.swift
+++ bigBrother/View/Create/VCreate.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class VCreate:View
 {
+    required init(controller: UIViewController)
+    {
+        super.init(controller:controller)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
