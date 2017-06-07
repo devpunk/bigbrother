572abc1aba902f9c990b0790474ebb8335c2c4df
vauxhall <zero.griffin@gmail.com>
2017-06-07 09:54:18 -0500

    button start layout

--- bigBrother/View/Create/VCreateBar.swift
+++ bigBrother/View/Create/VCreateBar.swift
@@ -3,6 +3,7 @@ import UIKit
 class VCreateBar:UIView
 {
     private weak var controller:CCreate!
+    private let kStartSize:CGFloat = 60
     
     init(controller:CCreate)
     {
@@ -11,10 +12,38 @@ class VCreateBar:UIView
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let buttonStart:UIButton = UIButton()
+        buttonStart.translatesAutoresizingMaskIntoConstraints = false
+        buttonStart.clipsToBounds = true
+        buttonStart.backgroundColor = UIColor.black
+        buttonStart.addTarget(
+            self,
+            action:#selector(actionStart(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
+        addSubview(buttonStart)
+        
+        NSLayoutConstraint.topToTop(
+            view:buttonStart,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:buttonStart,
+            toView:self)
+        NSLayoutConstraint.size(
+            view:buttonStart,
+            constant:kStartSize)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: actions
+    
+    func actionStart(sender button:UIButton)
+    {
+        
     }
 }
