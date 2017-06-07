5f62794bdbbb966c083bd071ba59e65e4d4451f4
vauxhall <zero.griffin@gmail.com>
2017-06-07 09:52:23 -0500

    added view bar to view create

--- bigBrother/View/Create/VCreate.swift
+++ bigBrother/View/Create/VCreate.swift
@@ -2,9 +2,34 @@ import UIKit
 
 class VCreate:View
 {
-    required init(controller: UIViewController)
+    private let kBarHeight:CGFloat = 120
+    
+    required init(controller:UIViewController)
     {
         super.init(controller:controller)
+        
+        guard
+            
+            let cCreate:CCreate = controller as? CCreate
+        
+        else
+        {
+            return
+        }
+        
+        let viewBar:VCreateBar = VCreateBar(controller:cCreate)
+        
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kBarHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
